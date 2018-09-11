344d9f5ef232b613b94e5100a5619d81c1407de7
Yvens 16 <yvensbelaston@gmail.com>
2018-09-11 09:44:13 +0200

    changes on the form

--- models/prof-model.js
+++ models/prof-model.js
@@ -6,9 +6,10 @@ const profSchema = new Schema (
   {
     lastName: { type: "String", require: true },
     firstName: { type: "String", require: true },
-    numéro: { type: 'String', require: true, match: /^(?:(?:\+|00)33|0)\s*[6-7](?:[\s.-]*\d{2}){4}$/},
+    number: { type: 'String', require: true, match: /^(?:(?:\+|00)33|0)\s*[6-7](?:[\s.-]*\d{2}){4}$/},
     email: { type: 'String', require:true, match: /^.+@.+\..+$/ },
-    encryptedPassword: { type: 'String',  }
+    encryptedPassword: { type: 'String' },
+    role: { type:'String', enum: [ 'normal', 'admin', 'prof'], required: true }
 },{
   timestamps: true
 }
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -73,4 +73,5 @@ router.get("/logout", (req, res, next) => {
   res.redirect("/");
 });
 
+
 module.exports = router;
