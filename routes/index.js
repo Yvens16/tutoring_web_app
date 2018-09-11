b25466d7581bc14c40b66da6556c447cebb534cc
Nerimene Akari <akari.nerimene@gmail.com>
2018-09-11 16:20:42 +0200

    settings

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,28 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/settings", (req, res, next) => {
+  if (!req.user) {
+    req.flash("error", "Tu dois être connecté pour accéder à tes paramètres");
+    res.redirect("/login");
+  } else {
+    res.render("settings-page");
+  }
+});
+
+router.post("/process-settings", (req, res, next) => {
+  const { lastname, firstname, phoneparent, phonestudent, email } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { $set: { lastname, firstname, phoneparent, phonestudent, email } },
+    { runValidators: true }
+  )
+    .then(userDoc => {
+      req.flash("success", "Modifications enregistrées");
+      res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
 module.exports = router;
